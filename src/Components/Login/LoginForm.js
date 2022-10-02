8201a096fa8b87b3bfecaf34d7cfdf4921cf82d7
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-10-02 15:17:51 -0300

    created userContexto to make login

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,39 +3,20 @@ import { Link } from 'react-router-dom'
 import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
-import { TOKEN_POST, USER_GET } from '../../api'
+import { UserContext } from '../../UserContext'
+
 
 const LoginForm = () => {
 
   const username = useForm();
   const password = useForm();
-
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (!token) return false
-    getUser(token)
-  }, [])
+  const { userLogin } = React.useContext(UserContext)
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (!username.validate() && !password.validate()) return false;
-    const { url, options } = TOKEN_POST({
-      username: username.value,
-      password: password.value
-    })
-    const response = await fetch(url, options);
-
-    const json = await response.json()
-    window.localStorage.setItem('token', json.token)
-    getUser(json.token)
-
-  }
-
-  const getUser = async (token) => {
-    const { url, options } = USER_GET(token)
-    const response = await fetch(url, options)
-    const json = await response.json()
-    console.log(json)
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
   }
 
   return (
